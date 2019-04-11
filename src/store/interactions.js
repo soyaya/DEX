852eebb8a219c5d90dc312621d4020fcbb61c97e
Gregory McCubbin <gwmccubbin@gmail.com>
2019-04-11 14:19:44 -0500

    13. Selectors

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -27,7 +27,7 @@ export const loadToken = async (web3, networkId, dispatch) => {
     dispatch(tokenLoaded(token))
     return token
   } catch (error) {
-    window.alert('Contract not deployed to the current network. Please select another network with Metamask.')
+    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
     return null
   }
 }
@@ -38,7 +38,7 @@ export const loadExchange = async (web3, networkId, dispatch) => {
     dispatch(exchangeLoaded(exchange))
     return exchange
   } catch (error) {
-    window.alert('Contract not deployed to the current network. Please select another network with Metamask.')
+    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
     return null
   }
 }
